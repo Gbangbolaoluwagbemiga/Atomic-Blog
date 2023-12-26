9f85b2c778bea2169991b04a13dd25b0ae7e13f9
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2023-12-26 18:57:08 +0100

    List component init

--- src/App.jsx
+++ src/App.jsx
@@ -96,18 +96,7 @@ function FormAddPost({onAddPost}) {
   );
 }
 
-function List({posts}) {
-  return (
-    <ul>
-      {posts.map((post, i) => (
-        <li key={i}>
-          <h3>{post.title}</h3>
-          <p>{post.body}</p>
-        </li>
-      ))}
-    </ul>
-  );
-}
+
 
 function Archive({onAddPost}) {
   // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
