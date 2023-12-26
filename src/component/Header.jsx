cc9b8070929ab8fbaf744463d8dc7623a41ab4f8
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2023-12-27 00:37:54 +0100

    Navbar component refactoring

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -13,20 +13,6 @@ function Header({posts, onClearPosts, searchQuery, setSearchQuery}) {
   );
 
   return (
-    // <header>
-    //   <h1>
-    //     <span>⚛️</span>The Atomic Blog
-    //   </h1>
-    //   <div>
-    // <Results posts={posts} />
-    // <SearchPosts
-    //   searchQuery={searchQuery}
-    //   setSearchQuery={setSearchQuery}
-    // />
-    // <button onClick={onClearPosts}>Clear posts</button>
-    //   </div>
-    // </header>
-
     <nav className="navbar navbar-expand-lg">
       <div className="container-fluid">
         <h1 className="header">
